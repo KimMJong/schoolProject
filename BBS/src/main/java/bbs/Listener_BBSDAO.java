29e2d614212303e2b0a89ed1229965da0cbe7cc0
KimMJong <answhd43@naver.com>
2022-10-10 16:20:47 +0900

    2022-10-10

--- BBS/src/main/java/bbs/Listener_BBSDAO.java
+++ BBS/src/main/java/bbs/Listener_BBSDAO.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Listener_BBSDAO {
 	private Connection conn;
 	private ResultSet rs;
-	
+
 	// mysql에 접속하는 부분
 	public Listener_BBSDAO() {
 		try {
@@ -18,78 +18,107 @@ public class Listener_BBSDAO {
 			String dbPassword = "rlaanswhd1";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	//현재 시간
+
+	// 현재 시간
 	public String getDate() {
 		String SQL = "SELECT NOW()";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			rs = pstmt.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				return rs.getString(1);
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return ""; //데이터베이스 오류
+		return ""; // 데이터베이스 오류
 	}
-	
+
 	public int getNext() {
 		String SQL = "SELECT bbsID FROM Listener_BBS order by bbsID DESC";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				return rs.getInt(1)+1;
+			if (rs.next()) {
+				return rs.getInt(1) + 1;
 			}
-			return 1; //첫번째 게시물
-		}catch(Exception e) {
+			return 1; // 첫번째 게시물
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return -1; //데이터베이스 오류
+		return -1; // 데이터베이스 오류
 	}
+
+	
 	public int write(String bbsTitle, String userID, String bbsContent) {
 		String SQL = "INSERT INTO Listener_BBS VALUES (?,?,?,?,?,?,0)";
+		int point = getPoint(userID);
 		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			pstmt.setInt(1, getNext());
-			pstmt.setString(2, bbsTitle);
-			pstmt.setString(3, userID);
-			pstmt.setString(4, getDate());
-			pstmt.setString(5, bbsContent);
-			pstmt.setInt(6, 1);
 			
-			return pstmt.executeUpdate();
-		}catch(Exception e) {
+			if(point -10 >0) {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setInt(1, getNext());
+				pstmt.setString(2, bbsTitle);
+				pstmt.setString(3, userID);
+				pstmt.setString(4, getDate());
+				pstmt.setString(5, bbsContent);
+				pstmt.setInt(6, 1);
+				point(userID);
+				
+				return pstmt.executeUpdate();
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return -1; //데이터베이스 오류
+		return -1; // 데이터베이스 오류
 	}
-	public int point(String bbsTitle, String userID, String bbsContent) {
-		String SQL = "update User SET userPoint = userPoint-10";
+	public int getPoint(String userID) {
+		String SQL = "select userPoint from user where userID = ?";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			return pstmt.executeUpdate();
+			pstmt.setString(1, userID);
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				int point = rs.getInt(1);
+				System.out.println(point);
+				return point;
+			}
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		return -1; //데이터베이스 오류
+				
+		return -1;
 	}
-	
-	
-	//게시글 목록을 보여주는 arraylist함수
-	public ArrayList<Listener_BBS> getList(int pageNumber){
+	public int point(String userID) {
+		String SQL = "update User SET userPoint = userPoint-10 where userID = ?";
+		try {
+
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			return pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 데이터베이스 오류
+	}
+
+	// 게시글 목록을 보여주는 arraylist함수
+	public ArrayList<Listener_BBS> getList(int pageNumber) {
 		String SQL = "SELECT * FROM Listener_BBS WHERE bbsID < ? AND bbsAvailable = 1 order by bbsID DESC LIMIT 8";
 		ArrayList<Listener_BBS> list = new ArrayList<Listener_BBS>();
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			pstmt.setInt(1, getNext()-(pageNumber - 1 )*10);
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
 			rs = pstmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				Listener_BBS bbs = new Listener_BBS();
 				bbs.setBbsID(rs.getInt(1));
 				bbs.setBbsTitle(rs.getString(2));
@@ -100,34 +129,35 @@ public class Listener_BBSDAO {
 				bbs.setUserPoint(rs.getInt(7));
 				list.add(bbs);
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return list;
 	}
-	
+
 	public boolean nextPage(int pageNumber) {
 		String SQL = "SELECT * FROM Listener_BBS WHERE bbsID < ? AND bbsAvailable = 1";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			pstmt.setInt(1, getNext()-(pageNumber - 1 )*10);
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
 			rs = pstmt.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				return true;
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
-	//목록 보여주는 함수
+
+	// 목록 보여주는 함수
 	public Listener_BBS getBbs(int bbsID) {
 		String SQL = "SELECT * FROM Listener_BBS WHERE bbsID = ?";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			pstmt.setInt(1, bbsID);
 			rs = pstmt.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				Listener_BBS bbs = new Listener_BBS();
 				bbs.setBbsID(rs.getInt(1));
 				bbs.setBbsTitle(rs.getString(2));
@@ -138,11 +168,12 @@ public class Listener_BBSDAO {
 				bbs.setUserPoint(rs.getInt(7));
 				return bbs;
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
+
 	public int update(int bbsID, String bbsTitle, String bbsContent) {
 		String SQL = "UPDATE Listener_BBS SET bbsTitle = ?, bbsContent = ? WHERE bbsID =?";
 		try {
@@ -151,11 +182,12 @@ public class Listener_BBSDAO {
 			pstmt.setString(2, bbsContent);
 			pstmt.setInt(3, bbsID);
 			return pstmt.executeUpdate();
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return -1; //데이터베이스 오류
+		return -1; // 데이터베이스 오류
 	}
+
 	public int delete(int bbsID) {
 		String SQL = "UPDATE Listener_BBS SET bbsAvailable = 0 WHERE bbsID = ?";
 		try {
@@ -163,11 +195,10 @@ public class Listener_BBSDAO {
 			pstmt.setInt(1, bbsID);
 
 			return pstmt.executeUpdate();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return -1; // 데이터베이스 오류
 	}
-	
-	
+
 }
--- BBS/src/main/webapp/writeAction.jsp
+++ BBS/src/main/webapp/writeAction.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ page import="bbs.Listener_BBSDAO" %>
+<%@ page import="user.User" %>
 <%@ page import="java.io.PrintWriter" %>
 <% request.setCharacterEncoding("UTF-8"); %>
 <jsp:useBean id="bbs" class = "bbs.Listener_BBS" scope ="page"/>
@@ -38,6 +39,7 @@
 			}else{
 				//제대로 내용이 들어 있을 때
 				Listener_BBSDAO bbsDAO = new Listener_BBSDAO();
+				User user = new User();
 				int result = bbsDAO.write(bbs.getBbsTitle(),userID, bbs.getBbsContent());
 				if(result == -1 ){
 					PrintWriter script = response.getWriter();
@@ -46,12 +48,12 @@
 					script.println("history.back()");
 					script.println("</script>");
 				}else {
+					
 					PrintWriter script = response.getWriter();
 					script.println("<script>");
 					script.println("alert('성공적으로 작성 되었습니다.')");
 					script.println("location.href = 'listener_bbs.jsp'");
 					script.println("</script>");
-					bbsDAO.point(bbs.getBbsTitle(), userID, bbs.getBbsContent());
 				}
 			}
 		}
